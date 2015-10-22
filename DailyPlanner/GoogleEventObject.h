ab8c7d33cd5863a9951fcd704954baefdf0ecf7e
sohamghosh121 <ghosh.soham@gmail.com>
2015-10-22 13:12:28 +0800

    soham's intermediate changes

---
+++ DailyPlanner/GoogleEventObject.h
@@ -0,0 +1,13 @@
+//
+//  GoogleEventObject.h
+//  DailyPlanner
+//
+//  Created by Soham Ghosh on 21/10/15.
+//  Copyright (c) 2015 Ian Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleEventObject : NSObject
+
+@end
---
+++ DailyPlanner/GoogleEventObject.m
@@ -0,0 +1,13 @@
+//
+//  GoogleEventObject.m
+//  DailyPlanner
+//
+//  Created by Soham Ghosh on 21/10/15.
+//  Copyright (c) 2015 Ian Chen. All rights reserved.
+//
+
+#import "GoogleEventObject.h"
+
+@implementation GoogleEventObject
+
+@end
